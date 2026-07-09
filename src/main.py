2b4ccfbfef0c51275161f059ba85828a3e355fb4
b-flights <benlflightstops@gmail.com>
2026-07-09 16:35:18 -0400

    Refactor global variables into an application model class

--- src/main.py
+++ src/main.py
@@ -3,73 +3,79 @@ from typing import *
 import wx  # type: ignore
 from interface import appFrame
 
-NODE_RADIUS = 15
-root: doc_node = doc_node("", "", None)
-curr_node: doc_node = root
-active_node: Optional[doc_node] = None
-node_list: list = []
-panelSize = None
-max_depth: int = 0
 
+class appModel():
+    NODE_RADIUS: int
+    root: doc_node
+    curr_node: doc_node
+    active_node: Optional[doc_node]
+    node_list: list
+    panelSize: "wx.Point"
+    max_depth: int
 
-# Set the horizontal start positions of each node's visual strip
-def update_start_positions():
-    root.distribute_proportion()
-    node_queue = [root]
-    curr_queue_node = root
-    node_proportion = 0
+    def __init__(self):
+        self.NODE_RADIUS = 15
+        self.root = doc_node("", "", None)
+        self.curr_node = self.root
+        self.active_node = None
+        self.node_list = [self.root]
+        self.panelSize = None
+        self.max_depth = 0
 
-    while not len(node_queue) == 0:
+    # Set the horizontal start positions of each node's visual strip
+    def update_start_positions(self):
+        self.root.distribute_proportion()
+        node_queue = [self.root]
+        curr_queue_node = self.root
         node_proportion = 0
-        curr_queue_node = node_queue.pop(0)
-        for node in curr_queue_node.children:
-            node_queue.append(node)
-            node.start = node.parent.start + node_proportion
-            node.y = node.depth / max_depth
-            node_proportion += node.proportion
 
-
-node_list = root.traverse()
-max_depth = root.get_max_depth()
+        while not len(node_queue) == 0:
+            node_proportion = 0
+            curr_queue_node = node_queue.pop(0)
+            for node in curr_queue_node.children:
+                node_queue.append(node)
+                node.start = node.parent.start + node_proportion
+                node.y = node.depth / self.max_depth
+                node_proportion += node.proportion
 
 
 class appFrameInst(appFrame):
+    model: appModel
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.model = appModel()
+
     def onUpdate(self, event):
-        curr_node.update_content(
+        self.model.curr_node.update_content(
             self.editCtrl.GetValue(), self.descCtrl.GetValue()
         )
         self.Refresh()
         self.Update()
 
     def onSaveRev(self, event):
-        global curr_node
-        global max_depth
-        global node_list
-        new_node = curr_node.add_new_ver(
+        new_node = self.model.curr_node.add_new_ver(
             self.editCtrl.GetValue(), self.descCtrl.GetValue()
         )
-        curr_node = new_node
-        node_list = root.traverse()
-        max_depth = root.get_max_depth()
-        update_start_positions()
+        self.model.curr_node = new_node
+        self.model.node_list = self.model.root.traverse()
+        self.model.max_depth = self.model.root.get_max_depth()
+        self.model.update_start_positions()
         self.Refresh()
 
     def onDelete(self, event):
-        global curr_node
-        global max_depth
-        global node_list
-        if curr_node.parent is not None:
-            parent_node = curr_node.parent
+        if self.model.curr_node.parent is not None:
+            parent_node = self.model.curr_node.parent
 
             for index, node in enumerate(parent_node.children):
-                if node == curr_node:
+                if node == self.model.curr_node:
                     parent_node.children.pop(index)
 
-            curr_node = parent_node
+            self.model.curr_node = parent_node
 
-        node_list = root.traverse()
-        max_depth = root.get_max_depth()
-        update_start_positions()
+        self.model.node_list = self.model.root.traverse()
+        self.model.max_depth = self.model.root.get_max_depth()
+        self.model.update_start_positions()
         self.Refresh()
 
     def onTreeToggle(self, event):
@@ -85,31 +91,28 @@ class appFrameInst(appFrame):
         self.Refresh()
 
     def onMouseMove(self, event):
-        global active_node
         new_node = None
-        for node in node_list:
+        for node in self.model.node_list:
+            x_pos = ((node.start + node.proportion / 2) * self.model.panelSize[0])
+            y_pos = ((node.depth + 1) / (self.model.max_depth + 2) * self.model.panelSize[1])
             if (
-                (((node.start + node.proportion / 2) * panelSize[0]) - event.GetX()) ** 2
-                + (((node.depth + 1) / (max_depth + 2) * panelSize[1]) - event.GetY()) ** 2
-            ) < NODE_RADIUS ** 2:
+                (x_pos - event.GetX()) ** 2 + (y_pos - event.GetY()) ** 2
+            ) < self.model.NODE_RADIUS ** 2:
                 new_node = node
 
-        if active_node != new_node:
-            active_node = new_node
+        if self.model.active_node != new_node:
+            self.model.active_node = new_node
             self.Refresh()
 
     def onTreeClick(self, event):
-        global curr_node
-        if active_node is not None:
-            curr_node = active_node
-            self.editCtrl.SetValue(active_node.content)
-            self.descCtrl.SetValue(active_node.desc)
+        if self.model.active_node is not None:
+            self.model.curr_node = self.model.active_node
+            self.editCtrl.SetValue(self.model.active_node.content)
+            self.descCtrl.SetValue(self.model.active_node.desc)
 
         self.Refresh()
 
     def onOpenDoc(self, event):
-        global root
-        global curr_node
         with wx.FileDialog(
             self, "Select .txt file", wildcard="Text files (*.txt)|*.txt",
             style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
@@ -120,22 +123,21 @@ class appFrameInst(appFrame):
             path = docOpenPrompt.GetPath()
             with open(path, 'r') as docFile:
                 doc_content = docFile.read()
-                root = doc_node(doc_content, "", None)
-                self.editCtrl.SetValue(root.content)
-                self.descCtrl.SetValue(root.desc)
-                curr_node = root
+                self.model.root = doc_node(doc_content, "", None)
+                self.editCtrl.SetValue(self.model.root.content)
+                self.descCtrl.SetValue(self.model.root.desc)
+                self.model.curr_node = self.model.root
                 self.Refresh()
 
     def onPaint(self, event):
-        global panelSize
         dc = wx.PaintDC(self.treePanel)
         dc.SetBackground(wx.Brush(wx.Colour(255, 255, 255)))
         dc.Clear()
         dc.SetPen(wx.Pen(wx.BLACK, 5))
-        panelSize = dc.GetSize()
-        max_depth = root.get_max_depth()
-        node_queue = [root]
-        curr_queue_node = root
+        self.model.panelSize = dc.GetSize()
+        self.model.max_depth = self.model.root.get_max_depth()
+        node_queue = [self.model.root]
+        curr_queue_node = self.model.root
 
         while not len(node_queue) == 0:
             curr_queue_node = node_queue.pop(0)
@@ -144,29 +146,29 @@ class appFrameInst(appFrame):
                 dc.SetPen(wx.Pen(wx.BLACK, 3))
                 dc.DrawLine(
                     int((curr_queue_node.start + curr_queue_node.proportion / 2)
-                        * panelSize[0]),
-                    int((curr_queue_node.depth + 1) / (max_depth + 2)
-                        * panelSize[1]),
+                        * self.model.panelSize[0]),
+                    int((curr_queue_node.depth + 1) / (self.model.max_depth + 2)
+                        * self.model.panelSize[1]),
 
-                    int((node.start + node.proportion / 2) * panelSize[0]),
-                    int((node.depth + 1) / (max_depth + 2) * panelSize[1]),
+                    int((node.start + node.proportion / 2) * self.model.panelSize[0]),
+                    int((node.depth + 1) / (self.model.max_depth + 2) * self.model.panelSize[1]),
                 )
             dc.SetPen(wx.Pen(wx.BLACK, 4))
 
             # Colour selected node
-            if curr_queue_node == curr_node:
+            if curr_queue_node == self.model.curr_node:
                 dc.SetPen(wx.Pen(wx.RED, 4))
 
             # Colour active node, takes priority if selected and active nodes are the same
-            if curr_queue_node == active_node:
+            if curr_queue_node == self.model.active_node:
                 dc.SetPen(wx.Pen(wx.BLUE, 4))
 
             dc.DrawCircle(
                 int((curr_queue_node.start + curr_queue_node.proportion / 2)
-                    * panelSize[0]),
-                int((curr_queue_node.depth + 1) / (max_depth + 2)
-                    * panelSize[1]),
-                NODE_RADIUS
+                    * self.model.panelSize[0]),
+                int((curr_queue_node.depth + 1) / (self.model.max_depth + 2)
+                    * self.model.panelSize[1]),
+                self.model.NODE_RADIUS
             )
 
 
